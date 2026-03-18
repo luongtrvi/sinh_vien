3352207e4b09596bcc856b68bdfa0baa528b182f
Luong Trieu Vi <vitttc2004@gmail.com>
2026-03-18 19:15:55 +0700

    commit one

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+// public/index.php
+// Nạp file autoload của Composer
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Luongtrieuvi\Bai01QuanlySv\Controllers\SinhvienController;
+// Simple Router
+$action = $_GET['action'] ?? 'index';
+$controller = new SinhvienController();
+switch ($action) {
+    case 'add':
+        $controller->add();
+        break;
+    case 'index':
+    default:
+        $controller->index();
+        break;
+}
